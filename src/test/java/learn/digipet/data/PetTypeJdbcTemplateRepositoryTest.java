3119d86ed2d0b586cc00b9484cc9cdb446f0e27a
qLirly <WarrenTimJ@Gmail.com>
2021-06-28 15:49:51 -0400

    test folder

---
+++ src/test/java/learn/digipet/data/PetTypeJdbcTemplateRepositoryTest.java
@@ -0,0 +1,16 @@
+package learn.digipet.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetTypeJdbcTemplateRepositoryTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
