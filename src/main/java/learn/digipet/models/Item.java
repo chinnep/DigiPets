2a0cbf1ee2fbd95caa681dbd68bffe9698264a07
qLirly <WarrenTimJ@Gmail.com>
2021-06-28 13:04:31 -0400

    Finished Item data layer and model

--- src/main/java/learn/digipet/models/Item.java
+++ src/main/java/learn/digipet/models/Item.java
@@ -1,4 +1,42 @@
 package learn.digipet.models;
 
 public class Item {
+
+    private int itemId;
+    private String name;
+    private String description;
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private double price;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
