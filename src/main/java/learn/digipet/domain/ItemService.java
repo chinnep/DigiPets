f31b5a7b1ecad90b50a62aeee3055a306aada764
Josh Frey <freyj3773@gmail.com>
2021-06-29 23:42:54 -0500

    ItemService

--- src/main/java/learn/digipet/domain/ItemService.java
+++ src/main/java/learn/digipet/domain/ItemService.java
@@ -1,4 +1,18 @@
 package learn.digipet.domain;
 
+import learn.digipet.data.ItemRepository;
+import learn.digipet.models.Item;
+
+import java.util.List;
+
 public class ItemService {
+
+    private final ItemRepository repository;
+
+    public ItemService(ItemRepository repository) { this.repository = repository; }
+
+    public List<Item> findAll() { return repository.findAll(); }
+
+    public Item findById(int itemId) { return repository.findById(itemId); }
+
 }
