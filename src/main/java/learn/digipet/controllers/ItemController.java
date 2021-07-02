7a9cf2511af21f15ae73c5ee63d1c95f37823a44
Josh Frey <freyj3773@gmail.com>
2021-07-02 00:14:06 -0500

    Touch Ups

--- src/main/java/learn/digipet/controllers/ItemController.java
+++ src/main/java/learn/digipet/controllers/ItemController.java
@@ -1,5 +1,24 @@
 package learn.digipet.controllers;
 
+import learn.digipet.domain.ItemService;
+import learn.digipet.models.Item;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService service;
+
+    public ItemController(ItemService service) { this.service = service; }
+
+    @GetMapping
+    public List<Item> findAll() { return service.findAll(); }
+
+    @GetMapping("/{itemId}")
+    public Item findById(@PathVariable int itemId) { return service.findById(itemId); }
 
 }
